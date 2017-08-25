81ce195c2249a9f6efe2fde681e1c7d7586c28d9
zhihaozheng <zzhengcn@gmail.com>
2017-08-25 00:53:00 -0400

    confirm Fig4d

--- temca2data/geometry_analysis/change_z_to_40nm.R
+++ temca2data/geometry_analysis/change_z_to_40nm.R
@@ -1,1 +1,29 @@
 
+# first run startup.R and functions.R
+
+# scale Z from 35nm to 40nm, now uPN is 40nm thickness
+uPN = nlapply(uPN, scale, center=FALSE, scale=c(1,1,35/40))
+
+pns_40 = nlapply(pns, scale, center=FALSE, scale=c(1,1,35/40))
+
+# update landmarks Z from 35nm to 40nm, and update the bridging transformation JFRC2013_FAFB13 accordingly
+elm.landmarks = elmr::elm.landmarks
+
+elm.landmarks$Z1 = scale(elm.landmarks$Z1, center=FALSE, scale=35/40)
+
+m <- rgl::identityMatrix()
+diag(m) <- c(nat::voxdims(nat.flybrains::JFRC2013),1)
+
+elmlm=elm.landmarks[elmr::elm.landmarks[,"Use"],]
+l0=data.matrix(elmlm[,c("X","Y","Z"), drop=F])
+l1=data.matrix(elmlm[, c("X1", "Y1", "Z1"), drop = F])
+JFRC2013_FAFB13 <- nat::reglist(tpsreg(l1, l0), m)
+nat.templatebrains::add_reglist(JFRC2013_FAFB13,
+                                reference = nat.flybrains::JFRC2013,
+                                sample = elmr::FAFB13)
+
+# figure to re-run:
+# Fig4D (checked), Fig5B & C, FigS7 B & C
+# Table S1 (NBLAST result need a transformation with 40nm thickness)
+
+
